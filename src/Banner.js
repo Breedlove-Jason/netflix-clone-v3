e1b5221eaa31ebc84c439b907d8c4f76488b9853
Jason Breedlove <j@jasonbreedlove.com>
2024-05-01 20:40:02 -0700

    pull in data from api and populate banner with movie data

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Banner.css";
+import axios from "./axios";
+import requests from "./Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        setMovie(
+            request.data.results[
+            Math.floor(Math.random() * request.data.results.length - 1)
+            ],
+        );
+        return request;
+        }
+        fetchData();
+    }, []);
+    console.log(movie);
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
@@ -10,21 +26,18 @@ function Banner() {
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fhjukkwcishwdbbo3vp5m.jpeg")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">{movie?.title || movie?.name || movie?.original}</h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `Nostrud occaecat consequat veniam do anim cupidatat ullamco id fugiat nisi amet consectetur incididunt. Incididunt ad cupidatat tempor cupidatat cupidatat duis qui excepteur minim sint nisi laboris. Ullamco dolor nostrud aliqua amet nostrud ullamco nostrud proident exercitation dolore. Irure ad voluptate ullamco Lorem labore. Ea nisi eu nisi esse proident adipisicing mollit magna. Minim excepteur minim ea labore adipisicing Lorem duis id eu fugiat exercitation voluptate. Est ullamco veniam ad cupidatat dolore cupidatat laborum labore reprehenderit amet proident adipisicing ut.`,
-            150,
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
