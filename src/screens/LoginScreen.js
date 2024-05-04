bd7fd7456c189841e2a3940a7832fa48891facd9
Jason Breedlove <j@jasonbreedlove.com>
2024-05-03 20:21:39 -0700

    finish login screen and sign up page with styling

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import "./LoginScreen.css"
+import React, { useState } from "react";
+import "./LoginScreen.css";
+import SignUpScreen from "./SignUpScreen";
 
 const LoginScreen = () => {
-    return (
-        <div className={"loginScreen"}>
-            <div className="loginScreen__background">
-                <img className="loginScreen__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="netflix_logo"/>
-                <button className={'loginScreen__button'}>Sign In</button>
-                <div className="loginScreen__gradient"/>
-                <div className="loginScreen__body">
-                    <>
-                <h1>Unlimited films, T.V., and more.</h1>
-                    </>
-                </div>
-            </div>
+  const [signIn, setSignIn] = useState(false);
+  return (
+    <div className={"loginScreen"}>
+      <div className="loginScreen__background">
+        <img
+          className="loginScreen__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt="netflix_logo"
+        />
+        <button
+          className={"loginScreen__button"}
+          onClick={() => setSignIn(true)}
+        >
+          Sign In
+        </button>
+        <div className="loginScreen__gradient" />
+        <div className="loginScreen__body">
+          {signIn ? (
+            <SignUpScreen />
+          ) : (
+            <>
+              <h1>See what's next</h1>
+              <h2>Watch anywhere. Cancel at any time.</h2>
+              <h3>
+                Ready to watch? Enter your email to create or restart your
+                membership.
+              </h3>
+              <div className={"loginScreen__input"}>
+                <form>
+                  <input type="email" placeholder="Email Address" />
+                  <button
+                    onClick={() => setSignIn(true)}
+                    className={"loginScreen__getStarted"}
+                  >
+                    GET STARTED
+                  </button>
+                </form>
+              </div>
+            </>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LoginScreen;
